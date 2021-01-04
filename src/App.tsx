2146bda82c33197fc668f3cf456040c781fdcc62
baomee-shi <git@baomeeshi.com>
2021-01-04 14:47:51 +0700

    remove unused DisclaimerModal

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { UseWalletProvider } from 'use-wallet'
-import DisclaimerModal from './components/DisclaimerModal'
 import MobileMenu from './components/MobileMenu'
 import TopBar from './components/TopBar'
 import FarmsProvider from './contexts/Farms'
@@ -47,7 +46,6 @@ const App: React.FC = () => {
 					</Route>
 				</Switch>
 			</Router>
-			<Disclaimer />
 		</Providers>
 	)
 }
@@ -71,25 +69,6 @@ const Providers: React.FC = ({ children }) => {
 			</UseWalletProvider>
 		</ThemeProvider>
 	)
-}
-
-const Disclaimer: React.FC = () => {
-	const markSeen = useCallback(() => {
-		localStorage.setItem('disclaimer', 'seen')
-	}, [])
-
-	const [onPresentDisclaimerModal] = useModal(
-		<DisclaimerModal onConfirm={markSeen} />,
-	)
-
-	useEffect(() => {
-		const seenDisclaimer = true // localStorage.getItem('disclaimer')
-		if (!seenDisclaimer) {
-			onPresentDisclaimerModal()
-		}
-	}, [])
-
-	return <div />
 }
 
 export default App
